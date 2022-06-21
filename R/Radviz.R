29c609a43c1efd814033c81a89b09d42352a957f
Puwen Tan <37172600+hmutpw@users.noreply.github.com>
2022-06-21 17:01:48 +0800

    Add files via upload

---
+++ R/Radviz.R
@@ -0,0 +1,171 @@
+
+
+
+#############
+# get Radviz object
+#############
+#' title
+#'
+#' @param input_frm A data.frame used to generate the Radviz object. The columns were items around the circle.
+#' @param anchors Dimensions used to generate the map. Default: all the columns from the \code{input_frm}.
+#' @param norm_data Whether to normalized data into 0 to 1? Default: TRUE.
+#' @param trim_ratio The percentage of values trimmed for every anchor. Used when \code{norm_data} is TRUE. Default: 0.005.
+#' @param optim_anchor Whether to optimize the anchor order? Default: TRUE.
+#' @param recenter User defined the start anchor. Default: NULL.
+#' @param text_color Text color of anchors, inherited from \code{\link{do.radviz}}. Default: black.
+#' @param text_size Text size of anchors, inherited from \code{\link{do.radviz}}. Default:8.
+#' @param ... Other parameters passed to \code{\link{do.radviz}}.
+#'
+#' @return getRadviz object
+#'
+#' @export
+#' @importFrom Radviz do.L make.S cosine do.optimRadviz get.optim recenter do.radviz
+
+getRadObj <- function(input_frm,
+                      anchors = colnames(input_frm),
+                      norm_data = TRUE,
+                      trim_ratio = 0.005,
+                      optim_anchor = TRUE,
+                      recenter = NULL,
+                      text_color = "black",
+                      text_size = 4, ...){
+  # check anchors
+  anchors <- intersect(anchors,colnames(input_frm))
+  if(!all(anchors %in% colnames(input_frm))){
+    not_in <- setdiff(anchors, colnames(input_frm))
+    stop("Anchors [",paste(not_in, collapse = ","),"] not found from columns of input_frm!")
+  }
+  exp_mat <- as.matrix(input_frm[,anchors])
+  if(!is.numeric(exp_mat)){
+    stop("The 'anchors' columns in 'input_frm' must be numeric!")
+  }
+  # data normalization
+  trans <- function(vec, cutoff=trim_ratio){
+    Radviz::do.L(vec, fun = function(x) quantile(x,c(cutoff,1-cutoff)))
+  }
+  if(norm_data){
+    exp_mat <- t(apply(exp_mat,1,function(x){(x-min(x))/(max(x)-min(x))}))
+    colnames(exp_mat) <- anchors
+    other_cols <-setdiff(colnames(input_frm), anchors)
+    if(length(other_cols)>0){
+      input_frm <- data.frame(exp_mat, input_frm[, other_cols, drop=FALSE], check.names=F)
+    }else{
+      input_frm <- exp_mat
+    }
+  }
+  #---generate springs
+  mat.S <- Radviz::make.S(anchors)
+  if(optim_anchor){
+    mat.sim <- Radviz::cosine(exp_mat)
+    optim.mat <- Radviz::do.optimRadviz(mat.S,mat.sim,iter=10,n=100)
+    mat.S <- Radviz::make.S(Radviz::get.optim(optim.mat))
+    if(!is.null(recenter) && length(recenter)==1 && (recenter %in% anchors)){
+      mat.S <- Radviz::recenter(mat.S, newc = recenter)
+    }
+  }
+  rv <- Radviz::do.radviz(x = input_frm,
+                          springs = mat.S,
+                          trans = trans,
+                          label.color = text_color,
+                          label.size = text_size,...)
+  rv
+}
+
+
+#############
+# add circle for Radviz
+#############
+addRadvizCircle <- function(rv, color="black"){
+  circle <- ggplot2::annotate("path", x=cos(seq(0, 2*pi, length.out=100)), y=sin(seq(0, 2*pi,length.out=100)), color = color)
+  rv$proj <- rv$proj+circle
+  rv
+}
+
+#############
+# do Radviz plot
+#############
+#' Perform visualization
+#' @param rv A \code{\link{getRadviz}} object generate from \code{\link{do.radviz}}.
+#' @param color_by Groups used to label the color.
+#' @param outline_circle Whether to add circle in figure?
+#' @param plot_type Figure types to plot.
+#' @param text_size Text size.
+#' @param point_size Label size of point in \code{\link{geom_point()}}.
+#' @param point_shape Label shape of point in \code{\link{geom_point()}}.
+#' @param ... Other input parameters.
+#'
+#' @return a ggplot object
+#'
+#' @import ggplot2
+#'
+#' @export
+do_RadvizEnrichPlot <- function(rv,
+                                color_by = NULL,
+                                outline_circle=TRUE,
+                                plot_type=c("point","density","hexagonal","buble"),
+                                point_size=0.5,
+                                point_shape=16,
+                                alpha=1,
+                                continuous_colors = continuous_colorbar(),
+                                ...){
+  plot_type <- match.arg(plot_type)
+  rv_data_frm <- as.data.frame(rv$proj$data)
+  if(outline_circle) rv <- addRadvizCircle(rv)
+  if(!is.null(color_by)){
+    color_by <- intersect(color_by, colnames(rv_data_frm))
+    if(length(color_by)!=1) stop("The 'color_by' shold only have one element!")
+  }
+  p <- rv$proj
+  if(plot_type=="point"){
+    #---point plot
+    if(is.character(point_shape) && point_shape %in% colnames(rv_data_frm)){
+      p <- p + geom_point(aes_string(color=color_by, shape=point_shape),
+                          size=point_size, alpha=alpha, na.rm = T)
+    }else if(is.numeric(point_shape)){
+      p <- p + geom_point(aes_string(color=color_by), shape=point_shape,
+                          size=point_size, alpha=alpha, na.rm = T)
+    }
+  }else if(plot_type=="density"){
+    #---density
+    p <- Radviz::smoothRadviz(rv)+geom_point(shape='.', alpha=alpha)
+  }else if(plot_type=="hexagonal"){
+    #---hexagonal
+    p <- Radviz::hexplot(rv)
+  }else if(plot_type=="buble"){
+    #---buble
+    p <- Radviz::bubbleRadviz(rv, group=color_by)
+  }
+  if(!is.null(color_by) && mode(rv_data_frm[[color_by]]) == "numeric"){
+    p <- p+ggplot2::scale_color_gradientn(colours = rev(continuous_colors))
+  }
+  p
+}
+
+##############
+#---calculate entropy from probability
+#############
+.H <- function(freqs, unit = c("log", "log2", "log10")){
+  unit <- match.arg(unit)
+  #freqs = freqs/sum(freqs)
+  H <- -sum(ifelse(freqs > 0, freqs * log(freqs), 0))
+  if (unit == "log2") H <- H/log(2)
+  if (unit == "log10") H <- H/log(10)
+  H
+}
+
+
+##############
+# color bars
+##############
+
+continuous_colorbar <- function(type = c('RdYlBu', 'RdYlGn',"PRGn","PiYG", "Oranges", "Purples", "Greens", "Greys"),
+                                n = 100){
+  type <- match.arg(arg = type)
+  col_num <- RColorBrewer::brewer.pal.info[type, 'maxcolors']
+  bre <- RColorBrewer::brewer.pal(n = col_num, name = type)
+  pal <- colorRampPalette(colors = bre)(n)
+  pal
+}
+
+
+
---
+++ R/SubLocViz.R
@@ -0,0 +1,97 @@
+
+
+
+#############
+# subcellular localization visualization
+#############
+
+#' Sub-cellular visualization
+#'
+#' @param gene_name Gene symbols used for visualization
+#' @param stage Stages used for visualization
+#' @param use_data use normalized data?
+#' @param plot_type type of plot?
+#' @param color_by which column used for visualization?
+#' @param show_gene_name Whether to show gene names?
+#' @param label_size The gene names size in figure
+#' @param point_size The point size
+#' @param point_shape The point shape
+#' @param continuous_colors The color of points
+#' @param text_size_circle the text color of outlier
+#' @param text_color_circle the color of outlier
+#' @param ... other parameters.
+#'
+#' @return a ggplot2 object
+#' @importFrom purrr map2
+#' @importFrom ggpubr ggarrange
+#' @import ggplot2
+#' @export
+#'
+#' @examples
+subLocViz <- function(gene_name,
+                      stage = c("D0","D3","D5"),
+                      use_data = c("scaled","ratio","raw"),
+                      plot_type = c("point","density","hexagonal","buble"),
+                      color_by = NULL,
+                      show_gene_name = FALSE,
+                      label_size = 2,
+                      point_size = 1,
+                      point_shape = 16,
+                      continuous_colors = continuous_colorbar(),
+                      text_size_circle = 3,
+                      text_color_circle = "purple",
+                      ...){
+  #---data preparation
+  data(ESC_RSL_data)
+  anchors <- c('CE',"ME","SNE","CBNE","PE")
+  stage <- match.arg(arg = stage, several.ok = TRUE)
+  use_data <- match.arg(arg = use_data)
+  plot_type <- match.arg(arg = plot_type)
+  data_list <- ESC_RSL_data[stage]
+  data_list <- lapply(X = data_list, FUN = function(x, gene){
+    x[x$gene_name %in% gene, ,drop=FALSE]
+  }, gene = gene_name)
+  if(use_data == "ratio"){
+    data_list <- lapply(X = data_list, FUN = function(x, anchor){
+      ratio <- t(apply(x[,anchors,drop=FALSE],1,function(x){x/sum(x)}))
+      x[,anchors] <- ratio
+      x
+    },anchor = anchors)
+  }else if(use_data == "scaled"){
+    data_list <- lapply(X = data_list, FUN = function(x, anchor){
+      scaled <- t(apply(x[,anchors,drop=FALSE],1,function(x){(x-min(x))/(max(x)-min(x))}))
+      x[,anchors] <- scaled
+      x
+    },anchor = anchors)
+  }
+  #---data visualization
+  rv_list <- lapply(X = data_list,
+                    FUN = getRadObj,
+                    anchors = anchors,
+                    norm_data = FALSE,
+                    optim_anchor = FALSE,
+                    text_color = text_color_circle,
+                    text_size = text_size_circle,
+                    ...)
+  Radviz_list <- lapply(X = rv_list,
+                        FUN = do_RadvizEnrichPlot,
+                        color_by = color_by,
+                        outline_circle = TRUE,
+                        plot_type = plot_type,
+                        point_size = point_size,
+                        point_shape = point_shape,
+                        ...)
+  if(show_gene_name){
+    gg_lab <- geom_text(mapping = aes(label=gene_name),size = label_size)
+    Radviz_list <- lapply(X = Radviz_list,
+                          FUN = function(x,gg_lab){
+                            x+gg_lab
+                          },gg_lab=gg_lab)
+  }
+  #---add title
+  titles <- names(Radviz_list)
+  Radviz_list <- purrr::map2(.x = Radviz_list, .y = titles, .f = function(x,y){
+    x+ggtitle(label = y)+theme(plot.title = element_text(hjust = 0.5,size = 12))
+  })
+  ggpubr::ggarrange(plotlist = Radviz_list, nrow = 1)
+}
